b5cef42f3eced5f22a180c1e6fce3c57c62978f8
root <root@DESKTOP-K52IES9.localdomain>
2022-07-15 05:50:21 +0100

    made some adjustmen

--- monty.h
+++ monty.h
@@ -1,117 +1,89 @@
-#ifndef __MONTY__H
-#define __MONTY__H
+#ifndef MONTY_H
+#define MONTY_H
 
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <ctype.h>
-#include <stdarg.h>
-#include <errno.h>
 
-#define FAILURE 0
-#define USAGE "USAGE: monty file\n"
-#define ERR_FILE "Error: Can't open file %s\n"
-#define ERR_OPCODE "L%d: unknown instruction %s\n"
-#define ERR_MALLOC "Error: malloc failed\n"
-#define ERR_PINT "L%d: can't pint, stack empty\n"
-#define ERR_PUSH "L%d: usage: push integer\n"
-#define ERR_POP "L%d: can't pop an empty stack\n"
-#define ERR_SWAP "L%d: can't swap, stack too short\n"
-#define ERR_ADD "L%d: can't add, stack too short\n"
-#define ERR_SUB "L%d: can't sub, stack too short\n"
-#define ERR_DIV "L%d: can't div, stack too short\n"
-#define ERR_MUL "L%d: can't mul, stack too short\n"
+/*--- Struct Definitions ---*/
+extern int push_arg;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-		int n;
-		struct stack_s *prev;
-		struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
 /**
- * struct instruction_s - opcode and its function
+ * struct instruction_s - opcoode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
-		char *opcode;
-		void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /**
- * struct data - paramater pattern
- * @file_ptr: file_ptr to opened file
- *
- * Description: parameter pattern
+ * struct global_variable - opcoode and its function
+ * @file: the opcode
+ * @push_arg: function to handle the opcode
+ * @buffer: pointer to
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
  */
-typedef struct data
+typedef struct global_variable
 {
-	FILE *fp;
-	stack_t *stack;
-	unsigned int line_number;
-	char **words;
-	int num_words;
-	char *line;
-} data_t;
+    FILE *file;
+    int push_arg;
+    char *buffer;
+} global_var;
 
-#define INIT_DATA {NULL, NULL, 1, NULL, 0, NULL}
+extern global_var var_global;
 
-#include "lists.h"
+void read_file(char *filename, stack_t **stack);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+instruct_func get_op_func(char *str);
 
-/* interpreter.c */
-int interpret(int ac, char **av);
-void parse_opcodes();
-int exec_opcode(char *word);
+/*Fuxntions Monty*/
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
 
-/* stack.c */
-int push(stack_t **head, int n);
-int pop(stack_t **head);
-int peek(stack_t **head);
-int is_empty(stack_t **head);
-void pall(stack_t *head);
+/*Functions string ascci */
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
 
-/* data.c */
-data_t *data(void);
-void free_data(int all);
+/*Functions math*/
+void _sub(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
 
-/* token.c */
-char **strtow(char *str, char *d);
-int is_delim(char c, char *delim);
-void ffree(char **pp);
-int is_num(char *str);
-
-/* opcodes1.c */
-void opcode_push(stack_t **stack, unsigned int line_number);
-void opcode_pop(stack_t **stack, unsigned int line_number);
-void opcode_pint(stack_t **stack, unsigned int line_number);
-void opcode_pall(stack_t **stack, unsigned int line_number);
-void opcode_swap(stack_t **stack, unsigned int line_number);
-
-/* opcodes2.c */
-void opcode_nop(__attribute__((unused))stack_t **stack,
-	__attribute__((unused))unsigned int line_number);
-void opcode_add(__attribute__((unused))stack_t **stack,
-	__attribute__((unused))unsigned int line_number);
-void opcode_sub(__attribute__((unused))stack_t **stack,
-	__attribute__((unused))unsigned int line_number);
-void opcode_div(__attribute__((unused))stack_t **stack,
-	__attribute__((unused))unsigned int line_number);
-void opcode_mul(__attribute__((unused))stack_t **stack,
-	__attribute__((unused))unsigned int line_number);
-
+/*Utility functions*/
+void free_dlistint(stack_t *head);
+void dobfree(stack_t **stack);
+void dobfree2(stack_t **stack);
+int _isalpha(int c);
 #endif
